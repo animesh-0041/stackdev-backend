04586fc43f7a25fcf9c4a2123885e2696676a4e1
animesh-0041 <ghoroianimesh1508@gmail.com>
2024-09-16 19:48:23 +0530

    test dev backend

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const connection = mongoose.connect(process.env.MONGO_PROD_URL);
+const connection = mongoose.connect(process.env.MONGO_DEV_URL);
 
 module.exports = {
   connection,
